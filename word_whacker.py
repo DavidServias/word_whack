6eadc384be109d74a4b344e329e01f50a5afb4b4
DavidServias <davidservias@gmail.com>
2021-02-18 18:03:05 -0800

    add text to speech

--- word_whacker.py
+++ word_whacker.py
@@ -1,14 +1,18 @@
 import re
+import json
+import os
+import random
+import subprocess
 
 
 # story class
 class Story:
-    def __init__(self, title, story_text):
-        self.title = title
-        self.story_text = story_text
+    def __init__(self, story_data):
+        self.title = story_data["title"]
+        self.text = story_data["text"]
         # to insert a prompt in a story, enclose it with <prompt>
         # the following command will extract all of the prompts into a list
-        self.prompts = re.findall("<[\w\s]+>", self.story_text)
+        self.prompts = re.findall("<[\w\s]+>", self.text)
         self.user_input = []
 
     # gets a response from the user for each prompt, and saves it in user_input list.
@@ -20,18 +24,33 @@ class Story:
             self.user_input.append(response)
 
     def insert_user_input(self):
-        new_text = self.story_text
-        temp_input = self.user_input
-        for prompt in self.prompts:
-            # replace one occurrence of prompt with first item on temp_input list
-            new_text = re.sub(prompt, temp_input[0], new_text, 1)
-            # remove first item of temp_input list
-            del temp_input[0]
-        return new_text
+        if len(self.user_input) > 0:
+            new_text = self.text
+            temp_input = self.user_input
+            for prompt in self.prompts:
+                # replace one ocadsf
+                # currence of prompt with first item on temp_input list
+                new_text = re.sub(prompt, temp_input[0], new_text, 1)
+                # remove first item of temp_input list
+                del temp_input[0]
+            self.text = new_text
 
     def print_story(self):
         print(self.title,)
-        print(self.insert_user_input())
+        print(self.text)
+
+    def speak_story(self):
+        subprocess.call(["python", "Speech.py", self.text])
+
+        #  os.system("python Speech.py")
+        #  To do: use "subprocess" instead.
+
+
+def get_story():
+    f = open("Stories.json")
+    data = json.load(f)
+    new_story = data["stories"][0]
+    return Story(new_story)
 
 
 messages = {
@@ -40,7 +59,8 @@ messages = {
 }
 
 
-my_story = Story(my_title, my_story_text)
-
+my_story = get_story()
 my_story.get_user_input()
+my_story.insert_user_input()
 my_story.print_story()
+my_story.speak_story()
