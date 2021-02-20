c211295133a5e309020ad012d9341a7dd7fd9e3b
natcat <davidservias@gmail.com>
2021-02-20 12:16:00 -0800

    separate Story into it's own module

---
+++ Story.py
@@ -0,0 +1,48 @@
+import re
+from gtts import gTTS
+from playsound import playsound
+
+
+# story class
+class Story:
+    def __init__(self, story_data):
+        self.title = story_data["title"]
+        self.text = story_data["text"]
+        # to insert a prompt in a story, enclose it with <prompt>
+        # the following command will extract all of the prompts into a list
+        self.prompts = re.findall("<[\w\s]+>", self.text)
+        self.user_input = []
+
+    def test(self):
+        print(self.title)
+
+    def insert_user_input(self):
+        if len(self.user_input) > 0:
+            new_text = self.text
+            temp_input = self.user_input
+            for prompt in self.prompts:
+                # replace each occurrence of prompt with first item on temp_input list
+                new_text = re.sub(prompt, temp_input[0], new_text, 1)
+                # remove first item of temp_input list
+                del temp_input[0]
+            self.text = new_text
+
+    # gets a response from the user for each prompt, and saves it in user_input list.
+    def get_user_input(self):
+        print("Fill in the blanks:",)
+        for prompt in self.prompts:
+            prompt += ": "
+            response = input(prompt)
+            self.user_input.append(response)
+        self.insert_user_input()
+
+    def speak_story(self):
+        split_text = self.text.split(". ")
+        # Language in which you want to convert
+        language = 'en'
+
+        for line in split_text:
+            print(line + ".")
+            speech_audio = gTTS(text=line, lang=language, slow=False)
+            speech_audio.save("speech_audio.mp3")
+            playsound('speech_audio.mp3')
--- word_whacker.py
+++ word_whacker.py
@@ -2,63 +2,29 @@ import re
 import json
 from gtts import gTTS
 from playsound import playsound
+from Story import Story
 
 
-# story class
-class Story:
-    def __init__(self, story_data):
-        self.title = story_data["title"]
-        self.text = story_data["text"]
-        # to insert a prompt in a story, enclose it with <prompt>
-        # the following command will extract all of the prompts into a list
-        self.prompts = re.findall("<[\w\s]+>", self.text)
-        self.user_input = []
+def main():
+    stories = []
 
-    def insert_user_input(self):
-        if len(self.user_input) > 0:
-            new_text = self.text
-            temp_input = self.user_input
-            for prompt in self.prompts:
-                # replace each occurrence of prompt with first item on temp_input list
-                new_text = re.sub(prompt, temp_input[0], new_text, 1)
-                # remove first item of temp_input list
-                del temp_input[0]
-            self.text = new_text
+    def import_stories():
+        f = open("Stories.json")
+        data = json.load(f)
+        for story in data["stories"]:
+            stories.append(story)
+        if len(stories) < 0:
+            print("No stories found.")
 
-    # gets a response from the user for each prompt, and saves it in user_input list.
-    def get_user_input(self):
-        print(messages["user_input_prompt"])
-        for prompt in self.prompts:
-            prompt += ": "
-            response = input(prompt)
-            self.user_input.append(response)
-        self.insert_user_input()
+    def get_story():
+        if len(stories) <= 0:
+            import_stories()
 
-    def speak_story(self):
-        split_text = self.text.split(". ")
-        # Language in which you want to convert
-        language = 'en'
+        return Story(stories[0])
 
-        for line in split_text:
-            print(line + ".")
-            speech_audio = gTTS(text=line, lang=language, slow=False)
-            speech_audio.save("speech_audio.mp3")
-            playsound('speech_audio.mp3')
+    my_story = get_story()
+    my_story.get_user_input()
+    my_story.speak_story()
 
 
-def get_story():
-    f = open("Stories.json")
-    data = json.load(f)
-    new_story = data["stories"][0]
-    return Story(new_story)
-
-
-messages = {
-    "welcome": "Welcome to Wacky Story Maker",
-    "user_input_prompt": "Please give your response:"
-}
-
-
-my_story = get_story()
-my_story.get_user_input()
-my_story.speak_story()
+main()
